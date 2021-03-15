ddf2fb42f367a53d74a2ad890f57c76c3b31ac03
SimoneMagliano <77775924+SimoneMagliano@users.noreply.github.com>
2021-03-15 17:48:37 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,90 +1,89 @@
 <?php
+require './lib/JSONReader.php';
 
-require "./lib/JSONReader.php";
-require "./lib/searchFunctions.php";
-
-$taskList = JSONReader('./dataset/TaskList.json');
-
-if(isset($_GET['searchText']) && trim($_GET['searchText'])!==''){
-    $searchText=trim(filter_var($_GET['searchText'], FILTER_SANITIZE_STRING));
-
-    $taskList = array_filter($taskList, searchTextDichiarative($searchText));
-    
-    if(isset($_GET['status']) && ($_GET['status'])!=='all'){
-        $status=($_GET['status']);
-    
-        
-        $taskList= array_filter($taskList, searchStatus($status));
-        
-    }else{
-        
-        $status='all';
-    }
-}else{
-
-    $searchText='';
-
-    if(isset($_GET['status']) && ($_GET['status'])!=='all'){
-        $status=($_GET['status']);
-    
-        
-        $taskList= array_filter($taskList, searchStatus($status));
-        
-    }else{
-        
-        $status='all';
-    }
-}
+$elenco=JSONReader('./dataset/TaskList.json');
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Task List</title>
-    <link rel="stylesheet" href="./asset/style.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Taklist</title>
 </head>
 <body>
-    <form action="index.php">
-        <input type="text" name="searchText" value="<?=$searchText?>">
 
-        <button type="submit">Cerca</button>
-
-        <div id="status">
-            <input type="radio" name="status" id="progress" value="progress" <?php if (isset($status) && $status== "progress") echo "checked";?>>
-            <label for="progress">In corso</label> 
-            <input type="radio" name="status" id="done" value="done" <?php if (isset($status)&& $status== "done") echo "checked";?>> 
-            <label for="done">Fatto</label> 
-            <input type="radio" name="status" id="todo" value="todo" <?php if (isset($status)&& $status== "todo") echo "checked";?>>
-            <label for="todo">Da fare</label> 
-            <input type="radio" name="status" id="all" value="all" <?php if (isset($status)&& $status== "all") echo "checked";?>>
-            <label for="all">Tutto</label> 
+    <div class="container-fluid bg-secondary py-3 mb-3 text-light">
+        <div class="container">
+            <h1 class="display-1">Tasklist</h1>
+        </div>
+    </div>
+ <form action="index.php">
+    <div class="container">
+        <div class="input-group pb-3 my-1">
+            <label class="w-100 pb-1 fw-bold" for="searchText">Cerca</label>
+            <input id="searchText"  type="text" class="form-control" placeholder="attività da cercare">
+            <div class="input-group-append">
+              <button class="btn btn-primary" type="button">Invia</button>
+            </div>
         </div>
 
+        <div id="status-radio" class=" mb-3">
+            <div class="fw-bold pe-2 w-100">Stato attività</div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" value="option1">
+                <label class="form-check-label" >tutti</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio"   value="option1">
+                <label class="form-check-label" >da fare</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio"   value="option2">
+                <label class="form-check-label" >in lavorazione</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio"   value="option2">
+                <label class="form-check-label" >fatto</label>
+              </div>
+        </div>
+    
+        <section class="tasklist mt-3">
+            <h1 class="fw-bold fs-6">Elenco delle attività</h1>
+            <table class="table">
+                <tr>
+                    <th class="w-100">nome</th>
+                    <th class="text-center">stato</th>
+                    <th class="text-center">data</th>
+                </tr>
+                
+                <tr>
+                <?php 
+                    foreach ($elenco as $key => $task) { 
+                    
+                        $status = $task['status'];
+                        $taskName = $task['taskName'];
+                        $date= $task['expirationDate'];
+                     
+                ?>
+                    <td><?= $taskName ?> </td>
+                    <td class="text-center">
+                        <span class="badge bg-secondary text-uppercase"><?= $status ?></span>
+                    </td>
+              
+                    <td class="text-nowrap">
+                    <?= $date ?>
+                    </td>
+                </tr>
+                <?php } ?>
+                <tr>
+            </table>
+
+        </section>
+    </div>
     </form>
-
-    <p><?= "Risultato: "?><b><?=count($taskList)?></b><?=" task per: "?><b><?=$searchText?></b><?=" filtro: "?><b><?=$status?></b></p>
-
-    <ul>
-        <?php
-        foreach ($taskList as $key => $task) { ?>
-
-            <?php
-            $status = $task['status'];
-            $taskName = $task['taskName'];
-            ?>
-            
-            <li class="tasklist-item tasklist-item-<?=$status?>">
-                <?=$taskName?> 
-                <b><?=$status?></b>
-            </li>
-        
-        <?php } ?>
-    </ul>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 </html>
